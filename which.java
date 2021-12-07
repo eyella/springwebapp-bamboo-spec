b8a41f508fe4d9288215036aba845225f8957805
Eathi Rajulu Yella <raju.yella@e2open.com>
2021-12-07 16:47:51 +0530

    Test

---
+++ which.java
@@ -0,0 +1,3 @@
+ 
+Use Refactor | Copy Class to create a class which is a copy of the selected class.
+This is useful when you need to create a class similar to an existing one, and it's not feasible to put shared functionality in a common superclass.
